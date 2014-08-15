c349e63e64da47170658c077bf58c6b6d0bcfdd6
psharma13 <pallav.sharma@teamaol.com>
2014-08-14 20:31:52 -0700

    edit item module optimization and Edit module now uses starActivityForResult rather startActivity

--- ToDoApp/src/com/example/todoapp/EditItemActivity.java
+++ ToDoApp/src/com/example/todoapp/EditItemActivity.java
@@ -1,11 +1,5 @@
 package com.example.todoapp;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.apache.commons.io.FileUtils;
-
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,10 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ListView;
 
 public class EditItemActivity extends ActionBarActivity {
-	private ArrayList<String> todoItems;
 	private EditText edItemText; 
 	private Integer pos;
 	private String editText;
@@ -25,21 +17,12 @@ public class EditItemActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_item);
 		edItemText = (EditText) findViewById(R.id.edItemText);
-		readItems();
 		editText = getIntent().getStringExtra(TodoActivity.TodoActivity_Item_DESC);
 		pos = getIntent().getExtras().getInt(TodoActivity.TodoActivity_Item_POS);
 		System.out.println("**** editText  : " + editText+" pos  : " + pos);		
 		edItemText.setText(editText);
 	}
-	private void readItems(){
-		File todoFile = new File(getFilesDir() ,   "todo.txt");
-		try{			
-			todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
-		}catch(IOException ex ){
-			todoItems = new ArrayList();
-			ex.printStackTrace();
-		}
-	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -61,18 +44,13 @@ public class EditItemActivity extends ActionBarActivity {
 	public void saveItem(View v){
 		String itemText = 	edItemText.getText().toString();
 		System.out.println("**** saveItem New Item --> " + itemText+" old item -->"+editText);
-		todoItems.set(pos, itemText);
-		saveItems();
-		Intent i = new Intent(EditItemActivity.this, TodoActivity.class);
-		startActivity(i); 
-	}
-	private void saveItems(){
-		File file = new File(getFilesDir(), "todo.txt");
-		try{
-			FileUtils.writeLines(file, todoItems);
-			readItems();
-		}catch(IOException ex ){
-			ex.printStackTrace();
-		}
-	}
+		// Prepare data intent 
+		Intent data = new Intent();
+		// Pass relevant data back as a result
+		data.putExtra(TodoActivity.TodoActivity_Item_DESC, itemText);
+		data.putExtra(TodoActivity.TodoActivity_Item_POS, pos);
+		// Activity finished ok, return the data
+		setResult(RESULT_OK, data); // set result code and bundle data for response
+		finish();
+	}	
 }
--- ToDoApp/src/com/example/todoapp/TodoActivity.java
+++ ToDoApp/src/com/example/todoapp/TodoActivity.java
@@ -1,14 +1,10 @@
 package com.example.todoapp;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.LineIterator;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -31,6 +27,8 @@ public class TodoActivity extends ActionBarActivity {
 	private ArrayAdapter<String> itemsAdapter;
 	private ListView lvItems;
 	private EditText etNewItem; 
+	// REQUEST_CODE can be any value we like, used to determine the result type later
+	private final int REQUEST_CODE = 20;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,11 +60,10 @@ public class TodoActivity extends ActionBarActivity {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				//readItems();
 				Intent i = new Intent(TodoActivity.this, EditItemActivity.class); 
-	              i.putExtra("TodoActivity_Item_POS", position);
-	              i.putExtra("TodoActivity_Item_DESC", todoItems.get(position));
-	              startActivity(i); 
+	              i.putExtra(TodoActivity_Item_POS, position);
+	              i.putExtra(TodoActivity_Item_DESC, todoItems.get(position));
+	              startActivityForResult(i, REQUEST_CODE);
 				return;
 			}
 			
@@ -100,7 +97,6 @@ public class TodoActivity extends ActionBarActivity {
 		File file = new File(getFilesDir(), "todo.txt");
 		try{
 			FileUtils.writeLines(file, todoItems);
-			//readItems();
 		}catch(IOException ex ){
 			ex.printStackTrace();
 		}
@@ -123,4 +119,19 @@ public class TodoActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      // REQUEST_CODE is defined above
+      if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+         // Extract TodoActivity_Item_DESC & TodoActivity_Item_POS value from result extras
+         String itemText = data.getExtras().getString(TodoActivity_Item_DESC);
+         int pos = data.getExtras().getInt(TodoActivity_Item_POS);
+         todoItems.set(pos, itemText);
+         saveItems();
+         itemsAdapter.notifyDataSetChanged();
+         }else{
+        	 System.out.println("**** resultCode is not ok --> " +resultCode);
+         }
+    } 
+
 }
